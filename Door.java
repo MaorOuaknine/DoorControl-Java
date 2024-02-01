da3d54102317052f2026385ba3e6fd390f1d19c2
MaorOuaknine <maorouaknine99@gmail.com>
2024-02-01 13:40:13 +0200

    Test : add class Door

---
+++ Door.java
@@ -0,0 +1,23 @@
+
+public class Door {
+    private boolean open;
+
+    public Door() {
+        this(true); }
+
+    public Door (boolean state) {
+        open=state;
+    }
+
+    public void open(){
+        open=true;
+    }
+
+    public void close() {
+        open=false;
+    }
+
+    public boolean isOpen() {
+        return open;
+    }
+}
